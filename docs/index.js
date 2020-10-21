eee2968400a1f944db7bd2ef09219f6533b1de4d
yuichi-sugiyama <yuichi-sugiyama@cybozu.co.jp>
2020-10-21 11:59:11 +0900

    gh-pages test

---
+++ docs/index.js
@@ -0,0 +1,78 @@
+const icon24 = `<svg 
+xmlns:dc="http://purl.org/dc/elements/1.1/"
+xmlns:cc="http://creativecommons.org/ns#"
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+xmlns:svg="http://www.w3.org/2000/svg"
+xmlns="http://www.w3.org/2000/svg"
+xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+height="26"
+width="26"
+xml:space="preserve"
+viewBox="0 0 26 26"
+y="0px"
+x="0px"
+id="レイヤー_1"
+version="1.1"
+inkscape:version="0.91 r13725"
+sodipodi:docname="kintone_2.svg"><sodipodi:namedview
+  pagecolor="#ffffff"
+  bordercolor="#666666"
+  borderopacity="1"
+  objecttolerance="10"
+  gridtolerance="10"
+  guidetolerance="10"
+  inkscape:pageopacity="0"
+  inkscape:pageshadow="2"
+  inkscape:window-width="1855"
+  inkscape:window-height="1056"
+  id="namedview10"
+  showgrid="false"
+  inkscape:zoom="8"
+  inkscape:cx="4.3125"
+  inkscape:cy="13"
+  inkscape:window-x="65"
+  inkscape:window-y="24"
+  inkscape:window-maximized="1"
+  inkscape:current-layer="レイヤー_1" /><metadata
+  id="metadata17"><rdf:RDF><cc:Work
+      rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+        rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title></dc:title></cc:Work></rdf:RDF></metadata><defs
+  id="defs15" /><style
+  id="style3"
+  type="text/css">
+ .st0{fill:#FCCF00;}
+ .st1{display:none;fill:#FFFFFF;}
+ .st2{display:none;fill:#003685;}
+</style><g
+  transform="matrix(0.69400631,0,0,0.69400631,1.7917981,5.7318612)"
+  id="g5"><path
+    style="fill:#fccf00"
+    id="path7"
+    d="M 32,7.1 C 29.8,5.8 27.6,5.4 25.2,5.9 c 0,-0.2 0,-0.3 0,-0.5 0,-2.5 -2,-4.5 -4.5,-4.5 -0.8,0 -1.6,0.2 -2.3,0.6 -1.1,-1 -2.6,-1.6 -4.2,-1.6 -2.8,0 -5.2,1.9 -6,4.4 C 7.6,4.2 6.9,4 6.1,4 2.9,4 0.3,6.7 0.3,9.9 c 0,2.4 1.5,4.5 3.6,5.4 0,3.7 3,6.7 6.7,6.7 1.8,0 3.5,-0.7 4.7,-1.9 0.7,0.4 1.4,0.6 2.3,0.6 1.8,0 3.4,-1.1 4.1,-2.6 2.7,-0.9 4.7,-3.4 4.7,-6.4 0,-0.8 -0.1,-1.6 -0.4,-2.3 1.6,-1.3 3.3,-1.9 6,-2.3"
+    class="st0"
+    inkscape:connector-curvature="0" /></g><path
+  style="display:none;fill:#ffffff"
+  id="path9"
+  d="m 310.6,85.9 c 0,30.5 -24.7,55.2 -55.2,55.2 -30.5,0 -55.2,-24.7 -55.2,-55.2 0,-30.5 24.7,-55.2 55.2,-55.2 30.5,0 55.2,24.7 55.2,55.2 z"
+  class="st1"
+  inkscape:connector-curvature="0" /><path
+  style="display:none;fill:#003685"
+  id="path11"
+  d="m 13.3,16 c -0.6,0 -1.1,-0.1 -1.4,-0.2 0,0.2 0.1,0.7 0.1,1.2 0,0.4 -0.1,0.8 -0.1,1.3 0.9,-0.2 2.8,-0.2 4.4,-0.2 0.4,0 0.4,0.2 0.4,0.3 0,0.1 0,0.2 0,0.3 0,0.5 0,1.1 0,1.9 0,0.7 0,1.7 0,3 -0.4,0.3 -0.9,0.5 -1.4,0.7 -0.5,0.2 -1,0.3 -1.4,0.4 -0.4,0.1 -0.8,0.1 -1.2,0.1 -0.3,0 -0.5,0 -0.6,0 -1.1,0 -2.2,-0.2 -3.2,-0.6 -1,-0.4 -2,-1 -2.8,-1.8 C 5.3,21.6 4.6,20.6 4.2,19.3 3.7,18.1 3.5,16.6 3.5,15 3.5,13.4 3.7,12 4.2,10.8 4.7,9.6 5.3,8.6 6,7.8 6.8,7 7.6,6.4 8.6,6 c 1,-0.4 2.1,-0.6 3.2,-0.6 1.1,0 2.1,0.1 2.8,0.4 0.8,0.3 1.4,0.6 1.9,1 0.5,0.4 0.9,0.8 1.2,1.2 0.3,0.4 0.6,0.8 0.7,1.1 l 0.3,0 C 18.8,8.8 18.9,8.4 19,7.9 19.1,7.4 19.4,6.7 19.7,6 18.6,5.4 17.4,5 15.9,4.7 14.6,4.2 13.2,4 11.7,4 9.7,4 8,4.3 6.5,4.9 5,5.5 3.8,6.3 2.8,7.3 1.8,8.3 1.1,9.5 0.7,10.8 0.2,12.1 0,13.4 0,14.9 c 0,1.7 0.3,3.2 0.8,4.5 0.6,1.4 1.3,2.6 2.3,3.6 1,1 2.2,1.7 3.6,2.3 1.4,0.5 2.9,0.8 4.6,0.8 0.8,0 1.6,0 2.3,-0.1 0.7,-0.1 1.4,-0.2 2.1,-0.4 0.7,-0.2 1.3,-0.3 2,-0.6 0.6,-0.2 2.1,0.1 2.1,-0.7 0,-0.9 0,-6.3 0,-7.8 C 19.8,16 19.3,16 19.3,16 L 19,16 c -1.3,0 -4,0.1 -5.7,0 z"
+  class="st2"
+  inkscape:connector-curvature="0" /></svg>`
+
+miro.onReady(() => {
+    miro.initialize({
+      extensionPoints: {
+        bottomBar: {
+          title: 'kintone',
+          svgIcon: icon24,
+          onClick: () => {
+            alert('Hi!')
+          }
+        }
+      }
+    })
+  })
